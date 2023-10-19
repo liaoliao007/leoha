a7cb4ad8e9ff68c9ff5af758632b6288beaa3800
liaoliao <372021030@qq.com>
2023-10-19 18:00:50 +0800

    add homepage

--- www/clock/__init__.py
+++ www/clock/__init__.py
@@ -1,1 +1,10 @@
 #https://blog.csdn.net/m0_63244368/article/details/128904156
+
+
+# git remote set-url origin https://<token>@github.com/liaoliao007/leoha.git
+
+#
+# 参数：
+# token：创建好的令牌号
+# username：gtihub起的名字
+# reponame：远程仓库的名字
